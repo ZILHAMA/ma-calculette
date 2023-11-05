0895c11198cf6aee684c84ec3f88c28574222da0
ilhem Benmeradi <ilhem.benmeradi@hap-s-hadj-ali.local>
2023-11-05 16:54:47 +0100

    ajout du java script

---
+++ Calculatrice/script.js
@@ -0,0 +1,5 @@
+var allbtns= document.querySelectorAll('input[type="button"]')
+
+function val(resultat){
+    form.fenetre.value=form.fenetre.value+resultat
+}
---
+++ app.js
@@ -0,0 +1,1 @@
+console.log('coucou cest moi');
