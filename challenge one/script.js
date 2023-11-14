04bb2b0001d8f9421483d29229b1a1e149c69d4b
ilhem Benmeradi <ilhem.benmeradi@hap-s-hadj-ali.local>
2023-11-14 16:54:52 +0100

    challenge one

---
+++ challenge one/script.js
@@ -0,0 +1,15 @@
+let message = "There is no war in Ba Sing Se"
+
+let words = message.split(" ")
+
+console.log("Number of words in the message: ", words.length)
+
+let countries = ['China','Japan','South Korea','Vietnam','Malaysia']
+let capital = ['Beijing','Tokyo','Seoul','Hanoï','Kuala Lumpur']
+
+for (let i = 0; i < countries.length; i++) {
+   
+
+    console.log(`la capitale de" ${countries[i]} est: ${capital[i]}`);
+}
+    
